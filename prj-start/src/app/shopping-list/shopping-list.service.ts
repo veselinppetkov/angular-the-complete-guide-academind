da4ecefa5653d3c3bd5402f9fe115542a22a0fa3
Veselin Petkov <90397928+veselinppetkov@users.noreply.github.com>
2022-03-16 17:15:09 +0200

    Using Services for Pushing Data from A to B

--- prj-start/src/app/shopping-list/shopping-list.service.ts
+++ prj-start/src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,4 @@
+import { EventEmitter } from "@angular/core";
 import { Ingredient } from "../shared/ingredient.model";
 
 export class ShoppingListService {
@@ -7,11 +8,16 @@ export class ShoppingListService {
         new Ingredient(`Dry Herbs`, 10),
             ];
 
+    ingredientsChanged = new EventEmitter<Ingredient[]>();
+
     getIngredients() {
         return this.ingredients.slice()
     }
 
     addIngredient(ingredient: Ingredient) {
         this.ingredients.push(ingredient)
+        this.ingredientsChanged.emit(this.ingredients.slice())
     }
+
+    
 };
