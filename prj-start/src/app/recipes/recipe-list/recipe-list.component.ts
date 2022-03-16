0ecc5099d01280f4282369e9828f0f0d7c6334a0
Veselin Petkov <90397928+veselinppetkov@users.noreply.github.com>
2022-03-16 16:19:54 +0200

    Managing Recipes in a Recipe Service

--- prj-start/src/app/recipes/recipe-list/recipe-list.component.ts
+++ prj-start/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,7 @@
+import { ThisReceiver } from '@angular/compiler';
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -9,14 +11,12 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
-  recipes: Recipe[] = [
-    new Recipe(`Miso Ramen`,`A step-by-step guide to getting this universally adored noodle soup just right.`, `https://www.forkknifeswoon.com/wp-content/uploads/2014/10/simple-homemade-chicken-ramen-fork-knife-swoon-01.jpg`),
-    new Recipe(`Buffalo Chicken Wings in a Jar`,`This fun-sized take on the game day appetizer offers everything you love about Buffalo wings, minus the bones, messy frying, and any risk of your drunk friends dripping sauce all over the place. Instead of wings, we simply take chicken thighs, simmer them directly in butter 'n hot sauce, shred, and mix with celery. The blue cheese comes in the accompanying crostini: baguette slices toasted and spread with butter and blue cheese.`, `https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F7616507.jpg`)
-  ];
+  recipes: Recipe[];
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onRecipeSelected(selectedRecipe: Recipe) {
