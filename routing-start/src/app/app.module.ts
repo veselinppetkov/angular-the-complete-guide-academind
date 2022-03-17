857cf537514c90c27609a459691bf2f5fee3e6d7
Veselin Petkov <90397928+veselinppetkov@users.noreply.github.com>
2022-03-17 17:14:05 +0200

    Updated app.module.ts

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -24,7 +24,7 @@ const appRoutes = [
     {path: ':id/edit', component: EditServerComponent}
 ]},
   {path: 'not-found', component: PageNotFoundComponent},
-  {path: '**', redirectTo: 'not-found'},
+  {path: '**', redirectTo: 'not-found', pathMatch: 'full'},
 ]
 
 @NgModule({
