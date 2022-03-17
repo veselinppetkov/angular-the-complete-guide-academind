cdaf55055604ad3ed7061619a258b55f37a50437
Veselin Petkov <90397928+veselinppetkov@users.noreply.github.com>
2022-03-17 16:33:39 +0200

    Using Query Parameters

--- routing-start/src/app/servers/edit-server/edit-server.component.ts
+++ routing-start/src/app/servers/edit-server/edit-server.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -11,13 +12,19 @@ export class EditServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
   serverName = '';
   serverStatus = '';
+  allowEdit: boolean = false;
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
+
+    this.route.queryParams.subscribe((params: Params) => {
+      console.log(params)
+    this.allowEdit = params.allowEdit === 'true' ? true : false;
+    })
   }
 
   onUpdateServer() {
