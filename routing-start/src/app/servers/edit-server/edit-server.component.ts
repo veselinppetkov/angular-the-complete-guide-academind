710e216d45f6692925c74dac33c4380782116960
Veselin Petkov <90397928+veselinppetkov@users.noreply.github.com>
2022-03-18 14:31:44 +0200

    Small changes made

--- routing-start/src/app/servers/edit-server/edit-server.component.ts
+++ routing-start/src/app/servers/edit-server/edit-server.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { CanComponentDeactivate} from '../../services/can-deactivate-guard.service';
 
 import { ServersService } from '../servers.service';
 
@@ -8,7 +10,7 @@ import { ServersService } from '../servers.service';
   templateUrl: './edit-server.component.html',
   styleUrls: ['./edit-server.component.css']
 })
-export class EditServerComponent implements OnInit {
+export class EditServerComponent implements OnInit, CanComponentDeactivate {
   server: {id: number, name: string, status: string};
   serverName = '';
   serverStatus = '';
@@ -34,4 +36,14 @@ export class EditServerComponent implements OnInit {
     this.router.navigate([`../`], {relativeTo: this.route});
   }
 
+  canDeactivate(): Observable<boolean> | Promise<boolean> | boolean {
+  if (!this.allowEdit) {
+    return true;
+  }
+  if ((this.serverName !== this.server.name || this.serverStatus !== this.server.status) && !this.changesSaved) {
+    return confirm(`Do you want to discard the changes?`)
+  } else {
+    return true;
+    }
+  }
 }
--- routing-start/src/app/services/can-deactivate-guard.service.ts
+++ routing-start/src/app/services/can-deactivate-guard.service.ts
@@ -1,5 +1,6 @@
-import { Observable } from 'rxjs/Observable'
 import { ActivatedRouteSnapshot, CanDeactivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+
+import { Observable } from 'rxjs/Observable'
 
 export interface CanComponentDeactivate {
     canDeactivate: () => Observable<boolean> | Promise<boolean> | boolean;
