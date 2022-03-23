3e72f6a1d17195aaf5f036d26922a4ac45d250ce
Veselin Petkov <90397928+veselinppetkov@users.noreply.github.com>
2022-03-23 15:27:51 +0200

    Using subjects

--- obs-01-start/src/app/user/user.component.ts
+++ obs-01-start/src/app/user/user.component.ts
@@ -1,20 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   id: number;
+  private subActivated: Subscription;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private userService: UserService) {
   }
 
   ngOnInit() {
-    this.route.params.subscribe((params: Params) => {
+    this.subActivated = this.route.params.subscribe((params: Params) => {
       this.id = +params.id;
     });
+  }
+
+  onActivate() {
+    this.userService.activatedEmmiter.next(true);
+  }
+
+  ngOnDestroy() {
+    this.subActivated.unsubscribe();
   }
 }
