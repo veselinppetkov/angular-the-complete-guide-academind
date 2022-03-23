b0a435947ce120b0bdf2ce984f20bab0097de6b8
Veselin Petkov <90397928+veselinppetkov@users.noreply.github.com>
2022-03-23 14:51:43 +0200

    Operators in use

--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Observable, Subscription } from 'rxjs';
+import { filter,map } from 'rxjs/operators'
 
 @Component({
   selector: 'app-home',
@@ -16,10 +17,10 @@ export class HomeComponent implements OnInit, OnDestroy {
     const customIntervalObs = Observable.create((observer) => {
       let count = 0;
       setInterval(() => {
-        if(count == 2) {
+        if(count == 20) {
           observer.complete()
         }
-        if (count == 3) {
+        if (count == 30) {
           observer.error(new Error(`It's enough!`))
         }
         observer.next(count);
@@ -27,7 +28,13 @@ export class HomeComponent implements OnInit, OnDestroy {
       }, 1000)
     })
 
-    this.ObsSubscrition = customIntervalObs.subscribe((counter) => {
+    this.ObsSubscrition = customIntervalObs.pipe(filter ((num:number) => {
+      if (num > 0) {
+        return num % 2 == 0;
+      }
+    }), map(el => {
+      return `Round: ${el}`
+    })).subscribe((counter) => {
       console.log(counter);
     }, (error) => {
       alert(error.message)
