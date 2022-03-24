33a953e0665d6c0852b54df4a408fb96e64f81e1
Veselin Petkov <90397928+veselinppetkov@users.noreply.github.com>
2022-03-24 14:40:04 +0200

    Assignment done

--- forms-td-assignment-start/src/app/app.component.ts
+++ forms-td-assignment-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  @ViewChild(`formData`) formData: NgForm;
+
+  onSubmit() {
+    console.log(this.formData.value)
+    this.formData.reset()
+  }
+
+  subscriptions: string[] = ["Basic", "Advanced", "Pro"]
+
 
 }
