9c57ea6d2fa209ddb649330f10a1ba4d5281572b
Veselin Petkov <90397928+veselinppetkov@users.noreply.github.com>
2022-03-28 15:31:51 +0300

    Assignment done

--- forms-reactive-assignment-start/src/app/app.component.ts
+++ forms-reactive-assignment-start/src/app/app.component.ts
@@ -1,9 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  projectStatus: string[] = ['Stable', 'Critical', 'Finished'];
+  projectForm: FormGroup;
+
+  ngOnInit() {
+    this.projectForm = new FormGroup({
+      'projectName': new FormControl(null, Validators.required, this.forbiddenName),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'projectStatus': new FormControl('Stable'),
+    })
+  }
+
+  forbiddenName(control: FormControl): Promise<any> | Observable<any> {
+
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'Test') {
+          resolve({ 'usernameIsForbidden': true });
+        } else {
+          resolve(null);
+        }
+      }, 1000)
+    });
+
+    return promise;
+  }
+
+  onSubmit() {
+    console.log(this.projectForm.value);
+    this.projectForm.reset();
+  }
 }
