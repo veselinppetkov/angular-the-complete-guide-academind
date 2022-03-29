59318ad509eca0aedcf862475a078e0e59d0d562
Veselin Petkov <90397928+veselinppetkov@users.noreply.github.com>
2022-03-29 15:06:55 +0300

    Adding new Ingredient Controls

--- prj-start/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ prj-start/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -59,6 +59,13 @@ export class RecipeEditComponent implements OnInit {
     return (<FormArray>this.recipeForm.get('ingredients')).controls;
   }
 
+  onAddIngredient() {
+    (<FormArray>this.recipeForm.get('ingredients')).push(new FormGroup({
+      'name': new FormControl(),
+      'amount': new FormControl()
+    }))
+  }
+
   onSubmit() {
     console.log(`It works just fine!`)
   }
