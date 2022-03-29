3f8265fc52051b93177de63349852b430621364b
Veselin Petkov <90397928+veselinppetkov@users.noreply.github.com>
2022-03-29 15:59:52 +0300

    Redirecting the User (after Deleting a Recipe)

--- prj-start/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ prj-start/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute,Params, Router } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { ShoppingListService } from 'src/app/shopping-list/shopping-list.service';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
@@ -13,24 +13,29 @@ export class RecipeDetailComponent implements OnInit {
   recipe: Recipe;
   id: number;
 
-  constructor(private shoppingListService: ShoppingListService, private route: ActivatedRoute, private recipeService: RecipeService, private router: Router) { 
+  constructor(private shoppingListService: ShoppingListService, private route: ActivatedRoute, private recipeService: RecipeService, private router: Router) {
 
   }
 
   ngOnInit(): void {
     this.route.params.subscribe((params: Params) => {
-    this.id = Number(params.id);
-    
-    this.recipe = this.recipeService.getRecipeById(this.id)
+      this.id = Number(params.id);
+
+      this.recipe = this.recipeService.getRecipeById(this.id)
     })
   }
 
   onEdit() {
-    this.router.navigate(['edit'], {relativeTo: this.route})
+    this.router.navigate(['edit'], { relativeTo: this.route })
   }
 
   onShoppingListClick() {
     this.shoppingListService.addRecipeIngredients(this.recipe.ingredients)
+  }
+
+  onDelete() {
+    this.recipeService.removeRecipe(this.id);
+    this.router.navigate(['../'], { relativeTo: this.route })
   }
 
 }
