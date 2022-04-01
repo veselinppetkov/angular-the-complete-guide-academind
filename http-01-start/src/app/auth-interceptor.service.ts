c5dcb76661af98755b248f9b16c67bc9025707e6
Veselin Petkov <90397928+veselinppetkov@users.noreply.github.com>
2022-04-01 10:56:23 +0300

    Introducing Interceptors

---
+++ http-01-start/src/app/auth-interceptor.service.ts
@@ -0,0 +1,10 @@
+import { HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+
+export class AuthInterceptorService implements HttpInterceptor {
+
+    intercept(req: HttpRequest<any>, next: HttpHandler) {
+        console.log(`It works just fine!`);
+        return next.handle(req);
+    }
+
+}
