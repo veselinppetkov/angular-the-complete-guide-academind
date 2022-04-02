0a0fff29f4923053f13604c4bff2fd11ec2b516a
Veselin Petkov <90397928+veselinppetkov@users.noreply.github.com>
2022-04-02 17:02:57 +0300

    Storing Recipes

--- prj-start/src/app/header/header.component.ts
+++ prj-start/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
@@ -7,10 +8,18 @@ import { Component, OnInit} from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   collapsed: boolean = true;
-  
-  constructor() {}
+
+  constructor(private dataStorageService: DataStorageService) { }
 
   ngOnInit(): void {
+  }
+
+  onSaveData() {
+    this.dataStorageService.storeData();
+  }
+
+  onFetchData() {
+    console.log(`To be continued....`)
   }
 
 
--- prj-start/src/app/shared/data-storage.service.ts
+++ prj-start/src/app/shared/data-storage.service.ts
@@ -1,7 +1,14 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { RecipeService } from "../recipes/recipe.service";
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient, private recipeService: RecipeService) { };
+
+    storeData() {
+        const recipes = this.recipeService.getRecipes();
+        this.http.put('https://ng-course-recipe-book-f1f0d-default-rtdb.europe-west1.firebasedatabase.app/recipes.json', recipes)
+            .subscribe(data => console.log(data));
+    }
 }
