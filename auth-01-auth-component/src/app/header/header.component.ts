5d01df13a45fb79d9f5026e853bde635c0833c46
Veselin Petkov <90397928+veselinppetkov@users.noreply.github.com>
2022-04-06 16:49:16 +0300

    Adding Logout

--- auth-01-auth-component/src/app/header/header.component.ts
+++ auth-01-auth-component/src/app/header/header.component.ts
@@ -31,4 +31,8 @@ export class HeaderComponent implements OnInit, OnDestroy {
   onFetchData() {
     this.dataStorageService.fetchRecipes().subscribe();
   }
+
+  onLogout() {
+    this.authService.logout();
+  }
 }
