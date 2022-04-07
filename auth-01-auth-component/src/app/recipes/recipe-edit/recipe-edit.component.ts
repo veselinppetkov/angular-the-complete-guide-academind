cc7516250dbc066aec4491de62a1b439e90b79ca
Veselin Petkov <90397928+veselinppetkov@users.noreply.github.com>
2022-04-07 18:42:59 +0300

    Ahead-of-Time Compilation

--- auth-01-auth-component/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ auth-01-auth-component/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -18,7 +18,7 @@ export class RecipeEditComponent implements OnInit {
     private route: ActivatedRoute,
     private recipeService: RecipeService,
     private router: Router
-  ) {}
+  ) { }
 
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
@@ -26,6 +26,10 @@ export class RecipeEditComponent implements OnInit {
       this.editMode = params['id'] != null;
       this.initForm();
     });
+  }
+
+  get ingredientsControls() {
+    return (this.recipeForm.get('ingredients') as FormArray).controls;
   }
 
   onSubmit() {
