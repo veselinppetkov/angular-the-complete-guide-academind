64716831651a6ab98cc11a665604c01d58a70a65
Veselin Petkov <90397928+veselinppetkov@users.noreply.github.com>
2022-04-07 15:00:37 +0300

    The ShoppingList Feature Module

---
+++ auth-01-auth-component/src/app/shopping-list/shopping-list.module.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { RouterModule, Routes } from "@angular/router";
+
+import { ShoppingEditComponent } from "./shopping-edit/shopping-edit.component";
+import { ShoppingListComponent } from "./shopping-list.component";
+
+const route: Routes = [{ path: 'shopping-list', component: ShoppingListComponent }];
+
+@NgModule({
+    declarations: [
+        ShoppingListComponent,
+        ShoppingEditComponent,
+    ],
+    imports: [RouterModule.forChild(route), CommonModule, FormsModule],
+    exports: [
+        ShoppingListComponent,
+        ShoppingEditComponent,
+    ],
+})
+
+export class ShoppingListModule { };
