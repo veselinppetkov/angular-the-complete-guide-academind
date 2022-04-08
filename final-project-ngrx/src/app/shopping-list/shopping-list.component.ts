02f1073ed0d540214823065c33d0b26546bf3220
Veselin Petkov <90397928+veselinppetkov@users.noreply.github.com>
2022-04-08 12:40:56 +0300

    Dispatching Actions

--- final-project-ngrx/src/app/shopping-list/shopping-list.component.ts
+++ final-project-ngrx/src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Observable, Subscription } from 'rxjs';
+import { Observable } from 'rxjs';
 
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
