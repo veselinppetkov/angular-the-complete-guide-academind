a0c761ee30be3e30bcd7349a7f6297fb4de04b77
Veselin Petkov <90397928+veselinppetkov@users.noreply.github.com>
2022-04-08 13:19:50 +0300

    Multiple Actions

--- final-project-ngrx/src/app/recipes/recipe.service.ts
+++ final-project-ngrx/src/app/recipes/recipe.service.ts
@@ -3,6 +3,8 @@ import { Subject } from 'rxjs';
 
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
+import * as ShoppingListActions from '../shopping-list/store/shopping-list.actions';
+import { Store } from '@ngrx/store';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable()
@@ -25,7 +27,7 @@ export class RecipeService {
   // ];
   private recipes: Recipe[] = [];
 
-  constructor(private slService: ShoppingListService) {}
+  constructor(private slService: ShoppingListService, private store: Store<{ shoppingList: { ingredients: Ingredient[] } }>) { }
 
   setRecipes(recipes: Recipe[]) {
     this.recipes = recipes;
@@ -41,7 +43,8 @@ export class RecipeService {
   }
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
-    this.slService.addIngredients(ingredients);
+    // this.slService.addIngredients(ingredients);
+    this.store.dispatch(new ShoppingListActions.AddIngredients(ingredients));
   }
 
   addRecipe(recipe: Recipe) {
